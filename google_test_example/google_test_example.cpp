fc57c9555a669f2aadb92577a71385625648a4b1
Phillip Schackier <CppPhil@users.noreply.github.com>
2021-11-25 18:57:27 +0100

    Initial Commit

---
+++ google_test_example/google_test_example.cpp
@@ -0,0 +1,12 @@
+﻿// google_test_example.cpp : Defines the entry point for the application.
+//
+
+#include "google_test_example.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
